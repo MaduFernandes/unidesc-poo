7f93964ea06bff2dffdc522efcf98fa552ffaee2
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-09-29 15:41:33 -0300

    feat: Adiciona Interface Aluno

--- src/br/com/unidesc/atividades/atividade02/Interfaces/IAluno.java
+++ src/br/com/unidesc/atividades/atividade02/Interfaces/IAluno.java
@@ -3,7 +3,7 @@ package br.com.unidesc.atividades.atividade02.Interfaces;
 public interface IAluno {
 
     void create();
-    void index();
+    void findAll();
     void update();
     void destroy();
 }
